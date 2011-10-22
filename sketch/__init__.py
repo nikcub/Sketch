36fd7331d1515de61d7b9c3eda20a5a8d9df23e3
Nik Cubrilovic <nikcub@gmail.com>
2011-10-23 00:53:34 +1100

    cleanup imports

--- sketch/__init__.py
+++ sketch/__init__.py
@@ -18,20 +18,8 @@ __version__ = '0.0.1'
 __author__ = "Nik Cubrilovic <nikcub@gmail.com>"
 __license__ = "BSD 3-clause"
 
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'sketch.django_settings'
-
-import os
 import sys
-
-# import cgi
-# import datetime
-# import time
-# import urllib
-# import urlparse
-# import webob
-# import webob.exc
-# from Cookie import BaseCookie
+import os
 
 import exception
 import serialize
@@ -46,20 +34,6 @@ from .session import Session
 from .model import Model
 from .application import Application
 from .users import User
-import debug
-
-from google.appengine.dist import use_library
-use_library('django', '0.96')
+from .debug import Handler as debug_handler
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'vendor'))
-from vendor.diffbot import DiffBot
-from vendor import jinja2
-
-from django.utils import simplejson as json
-
-# GAE
-# from google.appengine.ext import webapp
-# from google.appengine.ext import blobstore
-# from google.appengine.ext.webapp import template
-# from google.appengine.api import memcache
-# from google.appengine.runtime import DeadlineExceededError
