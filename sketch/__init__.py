86394588a1bec9d09a317805f3162cec762c836d
Nik Cubrilovic <nikcub@gmail.com>
2011-11-04 06:54:32 +1100

    removed django dep. added template helpers

--- sketch/__init__.py
+++ sketch/__init__.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Sketch
-    =======
+  Sketch
+  =======
 
-    __init__.py
+  A simple and portable python web application framework for Google App Engine
 
-    A simple and portable python web application framework
-
-    :homepage: <http://bitbucket.org/nik/sketch>
-    :author: Nik Cubrilovic <nikcub@gmail.com>
-    :copyright: (c) 2011 by Nik Cubrilovic and others, see AUTHORS for more details.
-    :license: 3-clause BSD, see LICENSE for more details.
+  :homepage: <http://bitbucket.org/nik/sketch>
+  :author: Nik Cubrilovic <nikcub@gmail.com>
+  :copyright: (c) 2011 by Nik Cubrilovic and others, see AUTHORS for more details.
+  :license: 3-clause BSD, see LICENSE for more details.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.1'
 __author__ = "Nik Cubrilovic <nikcub@gmail.com>"
 __license__ = "BSD 3-clause"
 
 import sys
 import os
+import logging
+
+VENDOR_DIR = os.path.join(os.path.dirname(__file__), 'vendor')
+if os.path.isdir(VENDOR_DIR):
+  sys.path.insert(0, VENDOR_DIR)
+else:
+  logging.exception("Error: %s" % VENDOR_DIR)
 
 import exception
-import serialize
 import util
+import jinja
+import db
 
 from .webapp import Request, Response, RequestHandler, RedirectHandler
 from .router import BaseRoute, SimpleRoute, Route, Router
 from .config import Config
 from .controllers import BaseController, AdminController
+from .rest import RestController
 from .messages import Messages
 from .session import Session
-from .model import Model
 from .application import Application
 from .users import User
 from .debug import Handler as debug_handler
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'vendor'))
+import serialize
